451f453d7d6797a3e851c87d7ebe10c7bb308cba
Oliver-Vandevelde <oliver.vandevelde7@gmail.com>
2019-02-20 09:38:13 +0100

    Pendu2

--- Jeu-du-pendu/pendu.js
+++ Jeu-du-pendu/pendu.js
@@ -4,36 +4,49 @@ document.getElementById("button").onclick = function() {guessLetter()};
 
 function guessLetter(){
     
-    let gagner = ['B','O','N','J','O','U','R'];
-    let pendu = ['_ ','_ ','_ ','_ ','_ ','_ ','_ ',];
+    let gagner = ['R','E','F','R','I','G','E','R','A','T','E','U','R'];
+    let pendu = [' _ ',' _ ',' _ ',' _ ',' _ ',' _ ',' _ ',' _ ',' _ ',' _ ',' _ ',' _ ',' _ '];
+    let erreur = []
     let lettre;
     
-    while (pendu.indexOf("_ ") != -1){
+    while (pendu.indexOf(" _ ") != -1){
         
-        alert("Trouver le mot suivant : \n"+pendu);
+        alert("Trouver le mot suivant : \n"+pendu+"\n\nMauvaise lettres : "+erreur);
         lettre = prompt("Choissisez une lettres.");
+        lettre = lettre[0];
         lettre = lettre.toUpperCase();
-        
+
         if (lettre == gagner[0]){
-            pendu.splice(0,1,'B');
+            pendu.splice(0,1,'R');
+            pendu.splice(3,1,'R');
+            pendu.splice(7,1,'R');
+            pendu.splice(12,1,'R');
         }
-        else if (lettre == gagner[1,4]){
-            pendu.splice(1,1,'O');
-            pendu.splice(4,1,'O');
+        else if (lettre == gagner[1]){
+            pendu.splice(1,1,'E');
+            pendu.splice(6,1,'E');
+            pendu.splice(10,1,'E');
         }
         else if (lettre == gagner[2]){
-            pendu.splice(2,1,'N');
+            pendu.splice(2,1,'F');
         }
-        else if (lettre == gagner[3]){
-            pendu.splice(3,1,'J');
+        else if (lettre == gagner[4]){
+            pendu.splice(4,1,'I');
         }
         else if (lettre == gagner[5]){
-            pendu.splice(5,1,'U');
+            pendu.splice(5,1,'G');
         }
-        else if (lettre == gagner[6]){
-            pendu.splice(6,1,'R');
+        else if (lettre == gagner[8]){
+            pendu.splice(8,1,'A');
+        }
+        else if (lettre == gagner[9]){
+            pendu.splice(9,1,'T');
+        }
+        else if (lettre == gagner[11]){
+            pendu.splice(11,1,'U');
         }
         else{
+            erreur.push(lettre);
             alert("La lettre "+lettre+" n'est pas une lettres valide")
         }
     }
